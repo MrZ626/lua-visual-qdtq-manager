35e8c84a9fbcb36819285bff07785a7332fef211
MrZ_26 <1046101471@qq.com>
2025-06-28 03:32:39 +0800

    允许选择其他包管理器

--- qdtq.lua
+++ qdtq.lua
@@ -1,10 +1,19 @@
+local manager = "paru"
+
+os.execute("clear")
+print("Press enter to continue with paru")
+print("Will execute: 'paru -Qdtq | -Qi <package> | -R <package>'")
+print("Type [name] if you want to use another package manager")
+manager = io.read():match("%S+") or manager
+
 local f
-local needRefresh = true
 local list
+local needRefresh = true
+
 while true do
     -- Refresh packages list
     if needRefresh then
-        f = io.popen("paru -Qdtq", 'r')
+        f = io.popen(manager .. " -Qdtq", 'r')
         list = {}
         while true do
             local line = f:read('*l')
@@ -18,21 +27,21 @@ while true do
     -- Print qdtq package names
     os.execute("clear")
     for i = 1, #list do print(i .. ": " .. list[i]) end
-    print("\nChoose a package to view details: (else to quit)")
+    print("\n[" .. manager .. "]\nChoose one to view details: (else to quit)")
     local choose = tonumber(io.read())
     if not list[choose] then os.exit() end
 
     -- Print package details
     os.execute("clear")
-    f = io.popen("paru -Qi " .. list[choose], 'r')
+    f = io.popen(manager .. " -Qi " .. list[choose], 'r')
     print(f:read('*a'))
     f:close()
 
     -- Remove package after confirmation
-    print("Type \"Y\" to uninstall: (else to return)")
+    print("\n[" .. manager .. "]\nType \"Y\" to uninstall: (else to return)")
     local confirm = io.read()
     if confirm == "Y" then
-        os.execute("paru -R " .. list[choose])
+        os.execute(manager .. " -R " .. list[choose])
         print("\nPackage " .. list[choose] .. " removed! Press Enter to continue...")
         io.read()
         needRefresh = true
