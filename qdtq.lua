d6a8598c0fb728172a9483d256e3ff7d90027428
MrZ_26 <1046101471@qq.com>
2025-06-28 03:05:27 +0800

    修R没用，优化性能

--- qdtq.lua
+++ qdtq.lua
@@ -1,36 +1,40 @@
-local f, o
-
+local f
+local needRefresh = true
+local list
 while true do
-    -- Print qdtq packages
-    os.execute("clear")
-    f = io.popen("paru -Qdtq", 'r')
-    local list = {}
-    while true do
-        local line = f:read('*l')
-        if not line then break end
-        table.insert(list, line)
+    -- Refresh packages list
+    if needRefresh then
+        f = io.popen("paru -Qdtq", 'r')
+        list = {}
+        while true do
+            local line = f:read('*l')
+            if not line then break end
+            table.insert(list, line)
+        end
+        f:close()
+        needRefresh = false
     end
-    for i = 1, #list do
-        print(i .. ": " .. list[i])
-    end
-    f:close()
 
+    -- Print qdtq package names
+    os.execute("clear")
+    for i = 1, #list do print(i .. ": " .. list[i]) end
     print("Choose a package to view details, anything else to quit.")
-    o = tonumber(io.read())
-    if not list[o] then os.exit() end
+    local choose = tonumber(io.read())
+    if not list[choose] then os.exit() end
 
     -- Print package details
     os.execute("clear")
-    f = io.popen("paru -Qi " .. list[o], 'r')
+    f = io.popen("paru -Qi " .. list[choose], 'r')
     print(f:read('*a'))
     f:close()
 
     -- Remove package after confirmation
     print("Type \"R\" to uninstall, anything else to continue...")
-    o = io.read()
-    if o == "R" then
-        os.execute("paru -R " .. list[o])
-        print("Package " .. list[o] .. " removed! Press Enter to continue...")
+    local confirm = io.read()
+    if confirm == "R" then
+        os.execute("paru -R " .. list[choose])
+        print("\nPackage " .. list[choose] .. " removed! Press Enter to continue...")
         io.read()
+        needRefresh = true
     end
 end
