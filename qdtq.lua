e1e82e6fbad1eab8e7499ac3752e08da73f705fb
MrZ_26 <1046101471@qq.com>
2025-06-28 03:39:52 +0800

    优化性能和交互

--- qdtq.lua
+++ qdtq.lua
@@ -1,9 +1,16 @@
 local manager = "paru"
 
 os.execute("clear")
-print("Press enter to continue with paru")
-print("Will execute: 'paru -Qdtq | -Qi <package> | -R <package>'")
-print("Type [name] if you want to use another package manager")
+print([[
+Visual -Qdtq Manager by MrZ
+
+Will execute:
+paru -Qdtq
+paru -Qi <package>
+paru -R <package>
+
+Press enter to continue with paru, or type [name] if you want to use another package manager
+]])
 manager = io.read():match("%S+") or manager
 
 local f
@@ -27,22 +34,20 @@ while true do
     -- Print qdtq package names
     os.execute("clear")
     for i = 1, #list do print(i .. ": " .. list[i]) end
-    print("\n[" .. manager .. "]\nChoose one to view details: (else to quit)")
+    print("\n[" .. manager .. "]\nChoose to view details: (else to quit)")
     local choose = tonumber(io.read())
     if not list[choose] then os.exit() end
 
     -- Print package details
     os.execute("clear")
-    f = io.popen(manager .. " -Qi " .. list[choose], 'r')
-    print(f:read('*a'))
-    f:close()
+    os.execute(manager .. " -Qi " .. list[choose])
 
     -- Remove package after confirmation
-    print("\n[" .. manager .. "]\nType \"Y\" to uninstall: (else to return)")
+    print("[" .. manager .. "]\nType \"Y\" to uninstall: (else to return)")
     local confirm = io.read()
     if confirm == "Y" then
         os.execute(manager .. " -R " .. list[choose])
-        print("\nPackage " .. list[choose] .. " removed! Press Enter to continue...")
+        print("\nPress Enter to continue...")
         io.read()
         needRefresh = true
     end
